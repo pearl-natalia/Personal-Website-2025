c563b1f909a4e63e2cd80f3a5a5845897c9496c4
Pearl <pnatalia@uwaterloo.ca>
2025-07-14 14:52:57 -0400

    Updating metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-// app/layout.tsx (or wherever your RootLayout lives)
-
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -8,10 +6,37 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pearl Natalia",
-  description: "My work.",
+  description: "My Personal Portfolio.",
+
+  metadataBase: new URL("https://pearlnatalia.com"),
+
   icons: {
-    icon: [{ url: "/images/favicon.png", type: "image/png" }],
-    shortcut: "/images/favicon.png",
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+
+  openGraph: {
+    title: "Pearl Natalia",
+    description: "My Personal Portfolio.",
+    url: "/",
+    siteName: "Pearl Natalia",
+    type: "website",
+    images: [
+      {
+        url: "/images/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Preview of Pearl Natalia’s portfolio",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Pearl Natalia",
+    description: "My Personal Portfolio.",
+    images: ["/images/preview.png"],
   },
 };
 
